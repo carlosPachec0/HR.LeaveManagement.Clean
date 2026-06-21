c0cff5dc02b3ee14af9e6caa30f4c2fa233a5754
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-06-21 00:43:32 -0700

    Service registration

---
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Application.MappingProfiles;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HR.LeaveManagement.Application;
+
+public static class ApplicationServiceRegistration
+{
+   public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+   {
+      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+      services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+      return services;
+   }
+}
