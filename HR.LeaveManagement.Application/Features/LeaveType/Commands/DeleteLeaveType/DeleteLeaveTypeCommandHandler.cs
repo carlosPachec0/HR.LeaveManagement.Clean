ff4e2d711a2829f29f71f6a731ef02b965e5c66a
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-06-21 01:27:11 -0700

    DeleteLeaveTypeCommand and handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+
+public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+   private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+   public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+   {
+      this._leaveTypeRepository = leaveTypeRepository;
+   }
+
+   public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+   {
+      // Validate incoming data
+
+      // retrieve domain entity object
+      var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+      // remove from database
+      await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+      // return record id
+      return Unit.Value;
+   }
+}
