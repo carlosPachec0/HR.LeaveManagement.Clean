ddeae5e0163e17da598e5aa6bfdf495f8714ca11
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-06-21 00:43:15 -0700

    LeaveTypeDto

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class LeaveTypeDto
+{
+   /// <summary>
+   /// Name property.
+   /// </summary>
+   public string Name { get; set; } = string.Empty;
+
+   /// <summary>
+   /// Name property.
+   /// </summary>
+   public int DefaultDays { get; set; }
+}
