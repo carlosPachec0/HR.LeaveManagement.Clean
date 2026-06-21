6bc20e54a8b37b439a0b1b32a4aa740684327682
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-06-21 15:16:50 -0700

    Change name

--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypesDetailQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypesDetailQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
 using MediatR;
 using System;
@@ -10,12 +11,12 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 
-public class GetLeaveTypesDetailsQueryHandler : IRequestHandler<GetLeaveTypesDetailsQuery, LeaveTypeDetailDto>
+public class GetLeaveTypesDetailQueryHandler : IRequestHandler<GetLeaveTypesDetailsQuery, LeaveTypeDetailDto>
 {
    private readonly IMapper _mapper;
    private readonly ILeaveTypeRepository _leaveTypeRepository;
 
-   public GetLeaveTypesDetailsQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+   public GetLeaveTypesDetailQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
    {
       _mapper = mapper;
       _leaveTypeRepository = leaveTypeRepository;
@@ -24,10 +25,13 @@ public class GetLeaveTypesDetailsQueryHandler : IRequestHandler<GetLeaveTypesDet
    public async Task<LeaveTypeDetailDto> Handle(GetLeaveTypesDetailsQuery request, CancellationToken cancellationToken)
    {
       // Query the database
-      var leaveTypes = await _leaveTypeRepository.GetByIdAsync(request.id);
-
+      var leaveType = await _leaveTypeRepository.GetByIdAsync(request.id);
+      if (leaveType == null)
+      {
+         throw new NotFoundException(nameof(LeaveType), request.id);
+      }
       // Convert data objects to DTO objects
-      var data = _mapper.Map<LeaveTypeDetailDto>(leaveTypes);
+      var data = _mapper.Map<LeaveTypeDetailDto>(leaveType);
 
       // Returns DTO object
       return data;
