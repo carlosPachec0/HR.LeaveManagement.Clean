6aa6683ab71d257c030870854b2b198e6b381192
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-06-23 14:26:19 -0700

    Refactor to IGenericRepository. Task<List<T>> GetAllAsync(); was replaced with Task<IReadOnlyList<T>> GetAllAsync();
    Write operations ain't return nothing

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -2,14 +2,14 @@
 {
    public interface IGenericRepository<T> where T : class
    {
-      Task<List<T>> GetAllAsync();
+      Task<IReadOnlyList<T>> GetAllAsync();
 
       Task<T> GetByIdAsync(int id);
 
-      Task<T> CreateAsync(T entity);
+      Task CreateAsync(T entity);
 
-      Task<T> UpdateAsync(T entity);
+      Task UpdateAsync(T entity);
 
-      Task<T> DeleteAsync(T entity);
+      Task DeleteAsync(T entity);
    }
 }
