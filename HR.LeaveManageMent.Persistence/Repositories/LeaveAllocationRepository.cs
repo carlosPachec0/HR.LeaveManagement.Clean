6d50e10c2e098cc57f71d0c85ef6affaee61e250
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-06-23 14:27:59 -0700

    Repositories implementations

---
+++ HR.LeaveManageMent.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManageMent.Persistence.DatabaseContext;
+
+namespace HR.LeaveManageMent.Persistence.Repositories;
+
+public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+{
+   public LeaveAllocationRepository(HrDatabaseContext hrDatabaseContext) : base(hrDatabaseContext)
+   {
+   }
+}
---
+++ HR.LeaveManageMent.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManageMent.Persistence.DatabaseContext;
+
+namespace HR.LeaveManageMent.Persistence.Repositories;
+
+public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+{
+   public LeaveRequestRepository(HrDatabaseContext hrDatabaseContext) : base(hrDatabaseContext)
+   {
+   }
+}
